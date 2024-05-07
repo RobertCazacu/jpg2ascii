19221dced76fd32abcbe73c81b464113dd15fdea
alex <alex@thequickbrownfox.ai>
2024-05-07 12:13:24 +0300

    added runner script

---
+++ run.py
@@ -0,0 +1,21 @@
+import argparse
+import img_reader
+
+
+
+
+def runner(args):
+    # Read the source image and return the grayscale matrix with values between 0-255
+    image = img_reader.read_img(args.source_path)
+    # Convert grayscale matrix to ASCII characters
+    
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--source_path', help='path to source image', required=True)
+    parser.add_argument('--target_path', help='path to target file')
+    args = parser.parse_args()
+
+    runner(args)
